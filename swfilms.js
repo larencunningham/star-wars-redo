b5530f2d122f9fa77c735153a74a2555646ada29
Laren Cunningham <55254847+larencunningham@users.noreply.github.com>
2019-12-12 12:16:11 -0700

    Update swfilms.js

--- swfilms.js
+++ swfilms.js
@@ -3,15 +3,15 @@ import { films } from './assests/films.js'
 let mainArea = document.querySelector('main')
 
 films.forEach(function(film) {
-    let filmDiv = document.createElement('div')
+    /*let filmDiv = document.createElement('div')
     let filmTitle = document.createElement('h1')
     let filmCrawl = document.createElement('p')
 
     filmTitle.textContent = film.title
-    filmCrawl.textContent = film.opening_crawl
+   filmCrawl.textContent = film.opening_crawl
 
     filmDiv.appendChild(filmTitle)
-    filmDiv.appendChild(filmCrawl)
+   filmDiv.appendChild(filmCrawl)*/
 
     mainArea.appendChild(filmDiv)
 });
